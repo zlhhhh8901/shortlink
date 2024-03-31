adcd7afcae4d2715fdeb677435e71c7a48a01a55
zlh <1502708341@qq.com>
2024-03-31 19:12:38 +0800

    feature：初始化短链接分组功能代码

---
+++ admin/src/main/java/com/zlh/shortlink/admin/dao/mapper/GroupMapper.java
@@ -0,0 +1,10 @@
+package com.zlh.shortlink.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zlh.shortlink.admin.dao.entity.GroupDO;
+
+/**
+ * 短链接分组数据层
+ */
+public interface GroupMapper extends BaseMapper<GroupDO> {
+}
---
+++ admin/src/main/java/com/zlh/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -0,0 +1,16 @@
+package com.zlh.shortlink.admin.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zlh.shortlink.admin.dao.entity.GroupDO;
+import com.zlh.shortlink.admin.dao.mapper.GroupMapper;
+import com.zlh.shortlink.admin.service.GroupService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 短链接分组接口实现层
+ */
+@Slf4j
+@Service
+public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+}
